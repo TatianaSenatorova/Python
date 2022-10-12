eb7e4738e95f1020bc613e1bd07d31cf366c8626
unknown <1975tanya@bk.ru>
2022-10-12 10:31:43 +0300

    hw_task_1

---
+++ seminar_fifth/hw_seminar_fifth/hw_1.py
@@ -0,0 +1,44 @@
+#1. Напишите программу, удаляющую из текста все слова, содержащие "абв". 
+#В тексте используется разделитель пробел.
+#in Number of words: 10
+#out
+#авб абв бав абв вба бав вба абв абв абв
+#авб бав вба бав вба
+
+from os import remove
+import random
+
+def fill_str(N: int):
+    list_words = []
+    for i in range(N):
+        word = random.sample("абв", 3)
+        list_words.append("". join(word))
+    return list_words
+
+def exept_some_words(list_words: list):
+     list_words.remove("абв")
+     str_1 = " ".join(list_words)
+     return str_1 
+      
+    
+# list_words_1 = fill_str(int(input("Enter a number of words: ")))
+# print(list_words_1)
+# print(f"list without 'абв' combination: {exept_some_words(list_words_1)}")
+
+
+# def fill_str(N: int):
+#     list_words = []
+#     for i in range(N):
+#         word = random.sample("абв", 3)
+#         list_words.append("". join(word))
+#     return list_words
+
+# def exept_some_words(list_words: list):
+#      list_words.remove("абв")
+#      str_1 = " ".join(list_words)
+#      return str_1 
+      
+    
+# list_words_1 = fill_str(int(input("Enter a number of words: ")))
+# print(list_words_1)
+# print(f"list without 'абв' combination: {exept_some_words(list_words_1)}")
