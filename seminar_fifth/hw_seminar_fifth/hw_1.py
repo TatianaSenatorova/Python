31d5a9b5f24ca78e788087d523f2d9f4cb86d708
unknown <1975tanya@bk.ru>
2022-10-13 15:24:56 +0300

    hw

--- seminar_fifth/hw_seminar_fifth/hw_1.py
+++ seminar_fifth/hw_seminar_fifth/hw_1.py
@@ -6,7 +6,7 @@
 #авб бав вба бав вба
 
 from os import remove
-import random
+import random  
 
 def fill_str(N: int):
     list_words = []
@@ -21,24 +21,9 @@ def exept_some_words(list_words: list):
      return str_1 
       
     
-# list_words_1 = fill_str(int(input("Enter a number of words: ")))
-# print(list_words_1)
-# print(f"list without 'абв' combination: {exept_some_words(list_words_1)}")
+list_words_1 = fill_str(int(input("Enter a number of words: ")))
+print(list_words_1)
+print(f"list without 'абв' combination: {exept_some_words(list_words_1)}")
 
 
-# def fill_str(N: int):
-#     list_words = []
-#     for i in range(N):
-#         word = random.sample("абв", 3)
-#         list_words.append("". join(word))
-#     return list_words
 
-# def exept_some_words(list_words: list):
-#      list_words.remove("абв")
-#      str_1 = " ".join(list_words)
-#      return str_1 
-      
-    
-# list_words_1 = fill_str(int(input("Enter a number of words: ")))
-# print(list_words_1)
-# print(f"list without 'абв' combination: {exept_some_words(list_words_1)}")
